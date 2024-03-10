0cd1fceb7b8f45f2c16fff6a21908abc7586b9a8
Niimq <nima.absi1381@gmail.com>
2024-03-10 19:29:02 -0400

    [DONE] No errors or anything. Build Completed.

--- Assets/Scripts/Level1/Elevator.cs
+++ Assets/Scripts/Level1/Elevator.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.SearchService;
+//using UnityEditor.SearchService;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour
