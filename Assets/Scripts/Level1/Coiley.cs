ed16c88d0ddc3b3e1719773db41c908ba84fff30
Niimq <nima.absi1381@gmail.com>
2024-04-03 01:53:15 -0400

    [MOD] Coiley's behaviour is mostly fixed
    he glitches again when I am on blockID 72. I need to debug and see it later why is it doing that.

--- Assets/Scripts/Level1/Coiley.cs
+++ Assets/Scripts/Level1/Coiley.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Coiley : MonoBehaviour
 {
@@ -72,8 +73,7 @@ public class Coiley : MonoBehaviour
                 {
                     MoveID = Qbert.GetComponent<QBert>().Blocks[i].GetComponent<Block>().transform;
                     CoileyLevel = Qbert.GetComponent<QBert>().Blocks[i].GetComponent<Block>().Level;
-                    transform.position = new Vector3(MoveID.position.x, MoveID.position.y + 0.35f);
-                    
+                    transform.position = new Vector3(MoveID.position.x, MoveID.position.y + 0.35f);                   
                 }
             }
         }
@@ -92,54 +92,13 @@ public class Coiley : MonoBehaviour
         if (activateCoileyEyes)
         {
 
-            int TargetID = Qbert.GetComponent<QBert>().LocationID;
-            int NextBlockIDA, NextBlockIDB;
+            CoileyDecides();
 
-            if (CoileyLevel >= Qbert.GetComponent<QBert>().LevelID)
-            {
-                NextBlockIDA = CoileyID / 3;
-                NextBlockIDB = CoileyID / 2;
-                if (TargetID % NextBlockIDA == 0 && TargetID % NextBlockIDB == 0)
-                {
-                    if (Random.value < 0.5f)
-                        CoileyID /= 2;
-                    else
-                        CoileyID /= 3;
-                }
-                else if (TargetID % NextBlockIDA == 0)
-                {
-                    CoileyID /= 3;
-                }
-                else
-                {
-                    CoileyID /= 2;
-                }
-            }
-            else
-            {
-                NextBlockIDA = CoileyID * 3;
-                NextBlockIDB = CoileyID * 2;
-                if (TargetID % NextBlockIDA == 0 && TargetID % NextBlockIDB == 0)
-                {
-                    if (Random.value < 0.5f)
-                        CoileyID *= 2;
-                    else
-                        CoileyID *= 3;
-                }
-                else if (TargetID % NextBlockIDA == 0)
-                {
-                    CoileyID *= 3;
-                }
-                else
-                {
-                    CoileyID *= 2;
-                }
-            }
             activateCoileyEyes = false;
         }
 
         if (Qbert != null && Qbert.GetComponent<QBert>() != null && Qbert.GetComponent<QBert>().Blocks != null)
-        {        
+        {
             for (int i = 0; i < Qbert.GetComponent<QBert>().Blocks.Count; i++)
             {
                 if (CoileyID == Qbert.GetComponent<QBert>().Blocks[i].GetComponent<Block>().blockID)
@@ -147,27 +106,116 @@ public class Coiley : MonoBehaviour
                     MoveID = Qbert.GetComponent<QBert>().Blocks[i].GetComponent<Block>().transform;
                     CoileyLevel = Qbert.GetComponent<QBert>().Blocks[i].GetComponent<Block>().Level;
 
-                    transform.position = MoveToPoint(new Vector3(MoveID.position.x, MoveID.position.y + 0.35f, MoveID.position.z));
+                    transform.position = MoveToPoint(new Vector3(MoveID.position.x, MoveID.position.y + 0.45f, MoveID.position.z));
 
-                    if (transform.position == new Vector3(MoveID.position.x, MoveID.position.y + 0.35f))
+                    if (transform.position == new Vector3(MoveID.position.x, MoveID.position.y + 0.45f, MoveID.position.z))
                     {
+                        Debug.Log("REACHED");
                         activateCoileyEyes = true;
                     }
                 }
             }
         }
-        
+
         Debug.Log(CoileyID);
+
         itCanMove = false;
-        yield return new WaitForSeconds(0.75f);
+        yield return new WaitForSeconds(0.25f);
         itCanMove = true;
+    }
+
+    void CoileyDecides()
+    {
+        int TargetID = Qbert.GetComponent<QBert>().LocationID;
+        float NextBlockIDA, NextBlockIDB;
+        bool QbertIsOnTheRight = false;
+
+        if (CoileyLevel > Qbert.GetComponent<QBert>().LevelID) // if coiley is lower than Qbert in the blocks
+        {
+            NextBlockIDA = CoileyID / 3;
+            NextBlockIDB = CoileyID / 2;
+            if (TargetID % NextBlockIDA == 0 && TargetID % NextBlockIDB == 0) // checking if coiley can jump down freely - but still he doesn't know which one to pick
+            {
+                if (QbertIsOnTheRight) // this is not working properly since it's only getting set when coiley reaches the same level with Qbert
+                    CoileyID /= 2;
+                else
+                    CoileyID /= 3;
+            }
+            else if (TargetID % NextBlockIDA == 0)
+            {
+                CoileyID /= 3;
+            }
+            else
+            {
+                CoileyID /= 2;
+            }
+        }
+
+        if (CoileyLevel == Qbert.GetComponent<QBert>().LevelID) // if == levels are same
+        {
+            if (CoileyID < Qbert.GetComponent<QBert>().LocationID) // Coiley is on the left.
+            {
+                QbertIsOnTheRight = true;
+                CoileyID /= 2 ;
+            }
+            else
+            {
+                QbertIsOnTheRight = false;
+                CoileyID /= 3;
+            }
+        }
+
+        if (CoileyLevel < Qbert.GetComponent<QBert>().LevelID)
+        {
+            NextBlockIDA = CoileyID * 3;
+            NextBlockIDB = CoileyID * 2;
+            if (TargetID % NextBlockIDA == 0 && TargetID % NextBlockIDB == 0)
+            {
+              
+                if (Random.value < 0.5f)
+                    CoileyID *= 2;
+                else
+                    CoileyID *= 3;
+            }
+            else if (TargetID % NextBlockIDA == 0)
+            {
+                CoileyID *= 3;
+            }
+            else if (TargetID % NextBlockIDB == 0)
+            {
+                CoileyID *= 2;
+            }
+            else
+            {
+                // in this case TargetID is smaller than nextBlockIDs
+                if (GetQbertOnTheLeft(NextBlockIDA, NextBlockIDB, TargetID))
+                {
+                    CoileyID *= 2;
+                }
+                else { CoileyID *= 3; }
+
+            }
+        }
+    }
+
+    bool GetQbertOnTheLeft(float IDA, float IDB, int TargetID)
+    {
+
+        if (IDA - TargetID < IDB - TargetID)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 
     Vector3 MoveToPoint(Vector3 point) // Making it move like so it won't teleport to the target
     {
-        return Vector3.MoveTowards(transform.position, point, 0.5f);
+        return Vector3.MoveTowards(transform.position, point, 0.1f);
     }
-
 
     //public void Respawn()
     //{
